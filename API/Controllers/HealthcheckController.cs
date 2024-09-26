ded71fa6bb93c9d2cada05f2487ab28ff1b7d47e
Nazg0r <gorchinshiy83@gmail.com>
2024-09-26 17:33:06 +0300

    Add healthcheck Controller
    Add healthcheck controller and implement GetStatus method

---
+++ API/Controllers/HealthcheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("{controller}")]
+	public class HealthcheckController: ControllerBase
+	{
+		[HttpGet]
+		public ActionResult GetStatus()
+		{
+			var currentDate = DateTime.Now.ToShortDateString();
+			var currentTime = DateTime.Now.ToLongTimeString();
+			var response = new { Date = currentDate,Time = currentTime, Status = "working" };
+
+			return Ok(response);
+		}
+	}
+}
