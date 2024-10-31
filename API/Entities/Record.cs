a90ae4a651f616d3e94b867a359d2cf3684b143c
Nazg0r <gorchinshiy83@gmail.com>
2024-10-31 20:18:21 +0200

    Add basic entities

---
+++ API/Entities/Record.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Entities
+{
+	public class Record
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public int CategoryId { get; set; }
+		public required string CreationDate { get; set; }
+		public int ExpensesSum { get; set; }
+
+		public required User User { get; set; }
+		public required Category Category { get; set; }
+	}
+}
