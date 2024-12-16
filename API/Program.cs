b95f580cfe73336d5650630e36bbdb6d8d61aa8a
Nazg0r <gorchinshiy83@gmail.com>
2024-12-16 23:41:29 +0200

    connect db to API

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
 using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<DataContext>();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+	opt.UseLazyLoadingProxies()
+	.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+});
 
 var app = builder.Build();
 
