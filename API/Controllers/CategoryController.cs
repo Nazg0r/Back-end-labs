5d05cb3c1e92670563f6d8a2b12a058aa2cf4182
Nazg0r <gorchinshiy83@gmail.com>
2024-12-16 23:48:55 +0200

    update category controller

--- API/Controllers/CategoryController.cs
+++ API/Controllers/CategoryController.cs
@@ -6,49 +6,45 @@ using System.Xml.Linq;
 
 namespace API.Controllers
 {
-	public class CategoryController : BaseApiController
+	public class CategoryController(DataContext dbContext) : BaseApiController
 	{
 		[HttpGet("{id}")]
-		public IActionResult GetCategory(int id)
+		public async Task<IActionResult> GetCategory(int id)
 		{
-			Category? category = DataContext.Categories.FirstOrDefault(c => c.Id == id);
+			Category? category = await dbContext.Categories.FindAsync(id);
 
-			if (category == null)
-			{
+			if (category is null)
 				return NotFound("Category is not found in system");
-			}
 
 			return Ok(category);
 		}
 
 		[HttpDelete("{id}")]
-		public IActionResult DeleteCategory(int id)
+		public async Task<IActionResult> DeleteCategory(int id)
 		{
-			Category? category = DataContext.Categories.FirstOrDefault(ctgry => ctgry.Id == id);
-			if (category == null)
-			{
+			Category? category = await dbContext.Categories.FindAsync(id);
+			if (category is null)
 				return NotFound("Category is not found in system");
-			}
-			DataContext.Categories.Remove(category);
+
+			dbContext.Categories.Remove(category);
+			await dbContext.SaveChangesAsync();
 
 			return Ok(category);
 		}
 
 		[HttpPost]
-		public IActionResult AddCategory(CategoryDTO category)
+		public async Task<IActionResult> AddCategory(CategoryDTO category)
 		{
-			if (DataContext.Categories.Any(ctgry => category.Name.ToLower() == ctgry.Name.ToLower()))
-			{
+			if (dbContext.Categories.Any(ctgry => category.Name.ToLower() == ctgry.Name.ToLower()))
 				return BadRequest("A category with this name already exists");
-			}
 
 			Category newCategory = new()
 			{
-				Id = DataContext.Categories[DataContext.Categories.Count - 1].Id + 1,
 				Name = category.Name
 			};
 
-			DataContext.Categories.Add(newCategory);
+			dbContext.Add(newCategory);
+			await dbContext.SaveChangesAsync();
 
 			return Ok(newCategory);
 		}
