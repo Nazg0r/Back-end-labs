0b94a50262ef979ef7c9ca1ae5e2877b31e45789
Nazg0r <gorchinshiy83@gmail.com>
2024-12-17 13:26:51 +0200

    add currency entity

---
+++ API/Entities/Currency.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities
+{
+	[PrimaryKey("Id")]
+	public class Currency
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
