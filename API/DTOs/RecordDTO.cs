8bf080aacb93467cf99bdede2760cceb24b6f926
Nazg0r <gorchinshiy83@gmail.com>
2024-12-17 13:29:34 +0200

    update DTOs

--- API/DTOs/RecordDTO.cs
+++ API/DTOs/RecordDTO.cs
@@ -5,5 +5,7 @@
 		public required int ExpensesSum { get; set; }
 		public required int UserId { get; set; }
 		public required int CategoryId { get; set; }
+		public string? Currency { get; set; } = "USD";
+
 	}
 }
--- API/DTOs/UserDTO.cs
+++ API/DTOs/UserDTO.cs
@@ -3,5 +3,6 @@
 	public class UserDTO
 	{
 		public required string Name { get; set; }
+		public string? Currency { get; set; } = "USD";
 	}
 }
