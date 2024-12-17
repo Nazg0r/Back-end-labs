1dc96437f14adbcc5867084e1350f6affb3d99a4
Nazg0r <gorchinshiy83@gmail.com>
2024-12-17 13:30:29 +0200

    add currency to db

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -8,6 +8,7 @@ namespace API.Data
 		public DbSet<User> Users { get; set; }
 		public DbSet<Record> Records { get; set; }
 		public DbSet<Category> Categories { get; set; }
+		public DbSet<Currency> Currencies { get; set; }
 
 	}
 }
